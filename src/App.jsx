1fafa925ebefb97d8f773f2a8b98d099bc7c1b10
Tine Lisec <tinelisec@Tines-MacBook-Pro.local>
2025-02-01 13:32:47 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,152 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const options = [
+  {
+    id: "soft",
+    title: "Soft - 3 min",
+    duration_mins: 3,
+  },
+  {
+    id: "medium",
+    title: "Medium - 5 min",
+    duration_mins: 5,
+  },
+  {
+    id: "hard",
+    title: "Hard - 7 min",
+    duration_mins: 7,
+  },
+  {
+    id: "extra_hard",
+    title: "Extra hard - 10 min",
+    duration_mins: 10,
+  },
+];
+
+function App() {
+  const [selected_ID, set_selected_ID] = useState("");
+  const [timeLeft, setTimeLeft] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    const selectedOption = options.find((opt) => opt.id === selected_ID);
+    if (selectedOption) {
+      setTimeLeft(selectedOption.duration_mins * 60);
+    }
+  }, [selected_ID]);
+
+  useEffect(() => {
+    let timer;
+    if (isRunning && timeLeft > 0) {
+      // Start the timer when it's running and timeLeft is greater than 0
+      timer = setInterval(() => {
+        setTimeLeft((prev) => prev - 1);
+      }, 1000);
+    } else if (timeLeft === 0) {
+      // Stop the timer when timeLeft reaches 0
+      setIsRunning(false);
+    }
+
+    return () => clearInterval(timer); // Cleanup the interval
+  }, [isRunning, timeLeft]);
+
+  const startTimer = () => {
+    if (!isRunning) {
+      const selectedOption = options.find((opt) => opt.id === selected_ID);
+      if (selectedOption) {
+        if (timeLeft === 0) {
+          // Reset timer if it reached 0
+          setTimeLeft(selectedOption.duration_mins * 60);
+        }
+        setIsRunning(true); // Start the timer
+      }
+    }
+  };
+
+  const resetTimer = () => {
+    const selectedOption = options.find((opt) => opt.id === selected_ID);
+    if (selectedOption) {
+      setTimeLeft(selectedOption.duration_mins * 60); // Reset to selected duration
+    }
+    setIsRunning(false); // Stop the timer
+  };
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${minutes}:${secs.toString().padStart(2, "0")}`;
+  };
+
+  return (
+    <div className="container">
+      <main>
+        <h1>Egg Timer</h1>
+
+        {/* options */}
+        <div className="options_container">
+          {options.map((option) => (
+            <button
+              key={option.id}
+              className={
+                "option " +
+                (selected_ID === option.id
+                  ? "selected"
+                  : selected_ID !== ""
+                  ? "notselected"
+                  : "")
+              }
+              onClick={() => set_selected_ID(option.id)}
+            >
+              {option.title}
+            </button>
+          ))}
+        </div>
+
+        {/* Timer and icons, Desktop*/}
+        <div className="timer_egg_container">
+          <img
+            src="src/assets/egg_thumbs_up.gif"
+            alt="Egg thumbs up"
+            className="egg_image"
+          />
+
+          <div className="timer_container">
+            <p className={isRunning ? "runningTimer timer" : "timer"}>
+              {formatTime(timeLeft)}
+            </p>
+
+            <hr />
+
+            <div className="controls_container">
+              <button className="control_btn" onClick={resetTimer}>
+                <img
+                  src="src/assets/icon_rewind.png"
+                  alt="Reset Icon"
+                  className="control_icon"
+                />
+              </button>
+              <button className="control_btn" onClick={startTimer}>
+                <img
+                  src="src/assets/icon_start.png"
+                  alt="Start Icon"
+                  className={
+                    isRunning ? "control_icon running" : "control_icon"
+                  }
+                />
+              </button>
+            </div>
+          </div>
+
+          <img
+            src="src/assets/egg_jump.gif"
+            alt="Egg jumping"
+            className="egg_image"
+          />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bg_color: "#FBF8EE",
+        brown_accent: "#6F5D4F",
+        button_outline: "#FFD963",
+        button_fill: "#FFFCEC",
+        button_fill_hover: "#FFF3C2",
+      },
+    },
+  },
+  plugins: [react()],
+});
