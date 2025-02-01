5e2425eaa4db5a54f15235367fffc4b1f2eb6dbf
T-Li <130779164+t-li-projects@users.noreply.github.com>
2025-02-01 18:48:35 +0100

    neki mejgnga

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/egg-timer/",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
